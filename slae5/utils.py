4631ba312cc08d0262fc17ab7526fa78165a7988
goodguyandy <goodfreeguyandy@gmail.com>
2020-08-13 13:38:59 +0200

    added files

---
+++ slae5/utils.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python2
+
+def littlen(s):
+    s = s.encode("hex")
+    s = bytearray.fromhex(s)
+    s.reverse()
+    return ''.join(format(x,'02x') for x in s).upper()
+    
+
+def pushstr(s):
+    s = littlen(s)
+    for i in range(0, len(s),8):
+        print "push " +  s[i:i+8] 
+
+def str2bytes(s):
+    s = s.encode("hex")
+    s = bytearray.fromhex(s)
+    return ''.join(format(x,'02x') for x in s).upper()
+ 
+
+
